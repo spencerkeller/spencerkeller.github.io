30f21d9042c76d6d5f3bcaffeff0a4eab30e1484
Spencer Keller <65426039+spencerkeller@users.noreply.github.com>
2020-08-14 16:26:37 -0700

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -28,7 +28,7 @@ function draw() {
   let tri_a = random(255);
   fill(tri_r,tri_g,tri_b,tri_a);
   triangle(
-    width*0.5 + random(-100,100), height*0.75 + random(-100,100), 
+    width*0.75 + random(-100,100), height*0.90 + random(-100,100), 
     width*0.5 + random(-100,100), height*0.75 + random(-100,100), 
     width*0.5 + random(-100,100), height*0.75 + random(-100,100));
 
@@ -38,7 +38,7 @@ function draw() {
   let tri_a2 = random(255);
   fill(tri_r2,tri_g2,tri_b2,tri_a2);
   triangle(
-    width*0.5 + random(-75,75), height*0.33 + random(-75,75), 
+    width*0.25 + random(-75,75), height*0.1 + random(-75,75), 
     width*0.5 + random(-75,75), height*0.33 + random(-75,75), 
     width*0.5 + random(-75,75), height*0.33 + random(-75,75));
 }
