cfa1909c25caaa618c2607eaa684dd7568d4dde5
Spencer Keller <spencerkeller.work@gmail.com>
2020-08-14 16:57:23 -0700

    merge

--- p5/sketch.js
+++ p5/sketch.js
@@ -10,8 +10,10 @@ var canvas;
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
+  canvas.style('display','block');
   canvas.style('z-index', '-1');
+  canvas.position(0,0);
+  
 }
 
 function draw() {
@@ -28,9 +30,9 @@ function draw() {
   let tri_a = random(255);
   fill(tri_r,tri_g,tri_b,tri_a);
   triangle(
-    width*0.5 + random(-100,100), height*0.75 + random(-100,100), 
-    width*0.5 + random(-100,100), height*0.75 + random(-100,100), 
-    width*0.5 + random(-100,100), height*0.75 + random(-100,100));
+    width*0.75 + random(-90,90), height*0.81 + random(-90,90), 
+    width*0.5 + random(-90,90), height*0.75 + random(-90,90), 
+    width*0.5 + random(-90,90), height*0.75 + random(-90,90));
 
   let tri_r2 = random(-5,5);
   let tri_g2 = random(-5,5);
@@ -38,11 +40,11 @@ function draw() {
   let tri_a2 = random(255);
   fill(tri_r2,tri_g2,tri_b2,tri_a2);
   triangle(
-    width*0.5 + random(-100,100), height*0.3 + random(-100,100), 
-    width*0.5 + random(-100,100), height*0.3 + random(-100,100), 
-    width*0.5 + random(-100,100), height*0.3 + random(-100,100));
+    width*0.25 + random(-75,75), height*0.27 + random(-75,75), 
+    width*0.5 + random(-75,75), height*0.33 + random(-75,75), 
+    width*0.5 + random(-75,75), height*0.33 + random(-75,75));
 }
 
 function windowResized(){
-  resizeCanvas(windowWidth,windowHeight)
+  resizeCanvas(windowWidth,windowHeight);
 }
